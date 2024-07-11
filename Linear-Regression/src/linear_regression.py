2fd16bb929c11d471cff1f715b7141ca6a2454c1
sadegh <sadegh15khedry@gmail.com>
2024-07-11 17:26:25 +0330

    get the custom model working

--- Linear-Regression/src/linear_regression.py
+++ Linear-Regression/src/linear_regression.py
@@ -10,19 +10,30 @@ class CustomLinearRegression:
         
     def fit(self, x, y):
         number_of_samples, number_of_features = x.shape
-        self.weights = np.zeros(number_of_features)
+        self.weights = np.zeros((number_of_features, 1))
         self.bias = 0
+        print(self.number_of_iterations)
         
-        for _ in range(number_of_samples):
+        x = np.array(x)
+        y = np.array(y)
+        
+        for i in range(self.number_of_iterations):
             y_pred = np.dot(x, self.weights) + self.bias
             
-            dw = (1/number_of_samples) * np.dot(x, y_pred - y)
+            dw = (1/number_of_samples) * np.dot(x.T, (y_pred - y))
             db = (1/number_of_samples) * np.sum(y_pred - y)
             
-            self.weights =  self.weights - self.learning_rate * dw
-            self.bias = self.bias - self.learning_rate * db
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+            
+            # Debugging: print the first few values of weights and bias at certain iterations
+            if i % 100 == 0:
+                print(f"Iteration {i}")
+                print(f"First few weights: {self.weights[:5].T}")
+                print(f"Bias: {self.bias}")
         
                 
     def predict(self, x):
+        x = np.array(x)
         y_pred = np.dot(x, self.weights) + self.bias
         return y_pred
--- Linear-Regression/src/model_training.py
+++ Linear-Regression/src/model_training.py
@@ -5,8 +5,10 @@ from linear_regression import CustomLinearRegression
 
 def train_model(x_train, y_train, number_of_iterations, learning_rate, model_type='custom'):
     if model_type == 'custom':
-        model = CustomLinearRegression(number_of_iterations, learning_rate)
-        
+        model = CustomLinearRegression(learning_rate, number_of_iterations)
+        model.fit(x_train, y_train)
+        return model
+    
     elif model_type == 'sklearn':
         model = LinearRegression ()
         model.fit(x_train, y_train)
