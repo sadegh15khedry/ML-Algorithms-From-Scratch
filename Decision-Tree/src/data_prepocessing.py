bbac39b07b11df5e680518fe44aba08ffac8bde2
sadegh <sadegh15khedry@gmail.com>
2024-07-25 09:41:31 +0330

    prepared data for decision tree

--- Decision-Tree/src/data_prepocessing.py
+++ Decision-Tree/src/data_prepocessing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
 
 def load_data(file_name):
     return pd.read_csv('../datasets/'+file_name)
@@ -13,3 +16,35 @@ def preprocess_data(df):
     df = df.dropna()
     df.drop_duplicates(keep=False) 
     return df    
+
+def normalize_data(df, method, normalization_columns):
+    if method == 'max_abs':
+        for column in normalization_columns:
+            df[column] = df[column] / df[column].abs().max()
+    elif method == 'min_max':
+        for column in normalization_columns: 
+            df[column] = (df[column] - df[column].min()) / (df[column].max() - df[column].min())     
+    elif method == 'z_score':
+        for column in normalization_columns: 
+            df[column] = (df[column] - df[column].mean()) / df[column].std()
+    elif method == 'robust':
+        for column in normalization_columns:
+            df[column] = (df[column] - df[column].median()) / (df[column].quantile(0.75) - df[column].quantile(0.25))
+    elif method == 'log':
+        for column in normalization_columns:
+            df[column] = np.log1p(df[column])  # log1p is used to avoid log(0)
+    elif method == 'l2':
+        df = df.apply(lambda x: x / np.sqrt(np.sum(np.square(x))), axis=1)
+    
+    return df  
+
+def categorical_encode(df, columns):
+    label_encoder = LabelEncoder()
+    
+    for column in columns:
+        if column in df.columns:
+            df[column] = label_encoder.fit_transform(df[column].astype(str))  # Ensure data is in string format
+        else:
+            print(f"Warning: Column '{column}' does not exist in DataFrame")
+    
+    return df
