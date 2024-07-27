fd341d85cdf5edd62805272bed937dbeef3823f7
sadegh <sadegh15khedry@gmail.com>
2024-07-27 14:55:02 +0330

    custom decision tree model working

--- Decision-Tree/src/model_training.py
+++ Decision-Tree/src/model_training.py
@@ -3,9 +3,9 @@ from sklearn import tree
 from decision_tree import CustomDecisionTree
 
 
-def train_model(x_train, y_train, model_type='custom'):
+def train_model(x_train, y_train, min_samples_split, max_depth, model_type='custom'):
     if model_type == 'custom':
-        model = CustomDecisionTree()
+        model = CustomDecisionTree(min_samples_split=2, max_depth=100, n_features=None)
         model.fit(x_train, y_train)
         return model
     
