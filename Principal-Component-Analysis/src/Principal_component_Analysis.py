2945079222545809cdc86c50aa8b717b3f240a89
sadegh <sadegh15khedry@gmail.com>
2024-08-05 16:56:01 +0330

    sklearn pca working

--- Principal-Component-Analysis/src/Principal_component_Analysis.py
+++ Principal-Component-Analysis/src/Principal_component_Analysis.py
@@ -1,3 +1,7 @@
 import numpy as np
 
 class CustomPCA:
+    def __init__(self,x):
+        pass
+    
+    
--- Principal-Component-Analysis/src/model_training.py
+++ Principal-Component-Analysis/src/model_training.py
@@ -1,17 +1,16 @@
-from sklearn.linear_model import LinearRegression 
+from sklearn.decomposition import PCA
+from principal_component_analysis import CustomPCA
 
-from linear_regression import CustomLinearRegression
 
-
-def train_model(x_train, y_train, number_of_iterations, learning_rate, model_type='custom'):
+def train_model(df, n_components=2, model_type='custom'):
     if model_type == 'custom':
-        model = CustomLinearRegression(learning_rate, number_of_iterations)
-        model.fit(x_train, y_train)
+        model = CustomPCA(n_components=2)
+        model.fit(df)
         return model
     
     elif model_type == 'sklearn':
-        model = LinearRegression ()
-        model.fit(x_train, y_train)
+        model = PCA(n_components=2)
+        model.fit(df)
         return model
 
 
