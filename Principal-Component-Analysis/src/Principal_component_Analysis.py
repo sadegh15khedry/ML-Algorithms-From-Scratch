db52ca08c1acedda2205060c40de9c26474310e1
sadegh <sadegh15khedry@gmail.com>
2024-08-05 17:18:43 +0330

    custom pca working

--- Principal-Component-Analysis/src/Principal_component_Analysis.py
+++ Principal-Component-Analysis/src/Principal_component_Analysis.py
@@ -1,7 +1,36 @@
 import numpy as np
 
 class CustomPCA:
-    def __init__(self,x):
-        pass
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.mean = None
+        self.components = None
+        
+    
+    def fit(self, x):
+        self.mean = np.mean(x, axis=0)
+        x = x - self.mean
+        
+        # Calculating the covariance
+        cov = np.cov(x.T)
+        
+        # Calculating the Eigenvectors
+        eigenvectors , eigenvalues = np.linalg.eigh(cov)
+        
+        eigenvectors = eigenvectors.T
+        
+        
+        #Srort eigenvectors
+        idxs = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+        
+        self.components = eigenvectors[:self.n_components]
+        
+    
+    def fit_transform(self, x, y=None):
+        x = x - self.mean
+        return np.dot(x, self.components.T)
+    
     
     
